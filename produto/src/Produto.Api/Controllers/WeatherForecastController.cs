67fe36d375104c482755cfee824b7a8e4b95b095
João André Martins Dias e Silva <jamdes@hotmail.com>
2023-01-03 22:31:17 -0300

    [wip]-controller

--- produto/src/Produto.Api/Controllers/WeatherForecastController.cs
+++ produto/src/Produto.Api/Controllers/WeatherForecastController.cs
@@ -25,8 +25,8 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public async Task<IEnumerable<WeatherForecast>> Get()
     {
-        Produto prod = new Produto() { Nome="Sprite lata",Descricao="Refrigerante Sprite Lata 320ml",CEST= "03.011.00",NCM= "2202.10.00", QtdItensContidos=1,CodigoBarras= "7894900010015", Tipo=ETipoProduto.MERCADORIA,MarcaId=new Guid("749b0477-379e-476c-86a7-b4f2e5db7ec3") };
-        var id=await _produtoRepository.Inserir(prod);
+        //Produto prod = new Produto() { Nome="Sprite lata",Descricao="Refrigerante Sprite Lata 320ml",CEST= "03.011.00",NCM= "2202.10.00", QtdItensContidos=1,CodigoBarras= "7894900010015", Tipo=ETipoProduto.MERCADORIA,MarcaId=new Guid("749b0477-379e-476c-86a7-b4f2e5db7ec3") };
+        //var id=await _produtoRepository.Inserir(prod);
         var teste=await _produtoRepository.ObterTodos(null);
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
