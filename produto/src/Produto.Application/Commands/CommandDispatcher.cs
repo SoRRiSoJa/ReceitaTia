96daddbbb467887d063b775c01928122e4d93b53
João André Martins Dias e Silva <jamdes@hotmail.com>
2023-01-04 00:26:34 -0300

    [wip]-camada de aplicação

---
+++ produto/src/Produto.Application/Commands/CommandDispatcher.cs
@@ -0,0 +1,27 @@
+﻿using Produto.Application.Exceptions;
+using Produto.Application.Interfaces;
+
+namespace Produto.Application.Commands
+{
+    public class CommandDispatcher : ICommandDispatcher
+    {
+        private readonly IServiceProvider _service;
+
+        public CommandDispatcher(IServiceProvider service)
+        {
+            _service = service;
+        }
+
+        public void Send<T>(T command) where T : ICommand
+        {
+
+            var handler = _service.GetService(typeof(ICommandHandler<T>));
+
+            if (handler != null)
+                ((ICommandHandler<T>)handler).Handle(command);
+            else
+                throw new NotFoundException($"Command doesn't have any handler {command.GetType().Name}");
+
+        }
+    }
+}
---
+++ produto/src/Produto.Application/Interfaces/ICommandHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace Produto.Application.Interfaces
+{
+    public interface ICommand
+    { }
+
+    public interface ICommandHandler
+    { }
+
+    public interface ICommandHandler<T> : ICommandHandler where T : ICommand
+    {
+        Task Handle(T command);
+    }
+
+    public interface ICommandDispatcher
+    {
+        void Send<T>(T command) where T : ICommand;
+    }
+
+}
