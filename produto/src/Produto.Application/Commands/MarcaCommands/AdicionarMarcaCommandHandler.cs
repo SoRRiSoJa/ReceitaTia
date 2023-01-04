ba5f09f342f422c083c5f5b3ca6cdd2c4d13bc01
João André Martins Dias e Silva <jamdes@hotmail.com>
2023-01-04 19:25:22 -0300

    [wip]retirado import

--- produto/src/Produto.Application/Commands/MarcaCommands/AdicionarMarcaCommandHandler.cs
+++ produto/src/Produto.Application/Commands/MarcaCommands/AdicionarMarcaCommandHandler.cs
@@ -8,14 +8,13 @@ namespace Produto.Application.Commands.Marca
 {
     public class AdicionarMarcaCommandHandler : IRequestHandler<AdcionarMarcaCommand, Guid>
     {
-        private readonly IMediator _mediator;
+        
         private readonly IMapper _mapper;
         private readonly IMarcaRepository _marcaRepository;
         private readonly IValidator<AdcionarMarcaCommand> _validator;
 
-        public AdicionarMarcaCommandHandler(IMediator _mediator,IValidator<AdcionarMarcaCommand> _validator, IMapper _mapper, IMarcaRepository _marcaRepository)
+        public AdicionarMarcaCommandHandler(IValidator<AdcionarMarcaCommand> _validator, IMapper _mapper, IMarcaRepository _marcaRepository)
         {
-            this._mediator = _mediator ?? throw new ArgumentNullException(nameof(_mediator));
             this._marcaRepository = _marcaRepository ?? throw new ArgumentNullException(nameof(_marcaRepository));
             this._mapper = _mapper ?? throw new ArgumentNullException(nameof(_mapper));
             this._validator = _validator ?? throw new ArgumentNullException(nameof(_validator));
