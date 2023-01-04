d8ad588f52ecb8274053f2a30a9a252f493e0fcc
João André Martins Dias e Silva <jamdes@hotmail.com>
2023-01-04 19:23:05 -0300

    [wip]- Mapper e Validator em ProdutoCommandhandler

--- produto/src/Produto.Application/Commands/MarcaCommands/AdicionarMarcaCommandHandler.cs
+++ produto/src/Produto.Application/Commands/MarcaCommands/AdicionarMarcaCommandHandler.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using FluentValidation;
 using MediatR;
 using Produtos.Domain.Repositories;
+using System.Reflection;
 
 namespace Produto.Application.Commands.Marca
 {
@@ -9,14 +11,24 @@ namespace Produto.Application.Commands.Marca
         private readonly IMediator _mediator;
         private readonly IMapper _mapper;
         private readonly IMarcaRepository _marcaRepository;
-        public AdicionarMarcaCommandHandler(IMediator _mediator, IMapper _mapper, IMarcaRepository _marcaRepository)
+        private readonly IValidator<AdcionarMarcaCommand> _validator;
+
+        public AdicionarMarcaCommandHandler(IMediator _mediator,IValidator<AdcionarMarcaCommand> _validator, IMapper _mapper, IMarcaRepository _marcaRepository)
         {
             this._mediator = _mediator ?? throw new ArgumentNullException(nameof(_mediator));
             this._marcaRepository = _marcaRepository ?? throw new ArgumentNullException(nameof(_marcaRepository));
             this._mapper = _mapper ?? throw new ArgumentNullException(nameof(_mapper));
+            this._validator = _validator ?? throw new ArgumentNullException(nameof(_validator));
         }
         public async Task<Guid> Handle(AdcionarMarcaCommand request, CancellationToken cancellationToken)
         {
+            var validation = await _validator.ValidateAsync(request);
+            
+            if (!validation.IsValid)
+            {
+                throw new ValidationException(validation.Errors);
+            }
+
             var marca = _mapper.Map<Produtos.Domain.Entities.Marca>(request);
             var idMarca = await _marcaRepository.Inserir(marca);
             return idMarca;
--- produto/src/Produto.Application/Commands/ProdutoCommands/AdicionarProdutoCommandHandler.cs
+++ produto/src/Produto.Application/Commands/ProdutoCommands/AdicionarProdutoCommandHandler.cs
@@ -1,4 +1,7 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using FluentValidation;
+using MediatR;
+using Produto.Application.Commands.Marca;
 using Produtos.Domain.Repositories;
 
 namespace Produto.Application.Commands.ProdutoCommands
@@ -6,24 +9,22 @@ namespace Produto.Application.Commands.ProdutoCommands
     public class AdicionarProdutoCommandHandler : IRequestHandler<AdcionarProdutoCommand, Guid>
     {
         private readonly IProdutoRepository _produtoRepository;
-        public AdicionarProdutoCommandHandler(IProdutoRepository _produtoRepository)
+        private readonly IMapper _mapper;
+        private readonly IValidator<AdcionarProdutoCommand> _validator;
+        public AdicionarProdutoCommandHandler(IProdutoRepository _produtoRepository, IValidator<AdcionarProdutoCommand> _validator, IMapper _mapper)
         {
             this._produtoRepository = _produtoRepository ?? throw new ArgumentNullException(nameof(_produtoRepository));
+            this._validator = _validator ?? throw new ArgumentNullException(nameof(_validator));
+            this._mapper = _mapper ?? throw new ArgumentNullException(nameof(_mapper));
         }
         public async Task<Guid> Handle(AdcionarProdutoCommand request, CancellationToken cancellationToken)
         {
-            var produto = new Produtos.Domain.Entities.Produto() 
-            { 
-                Nome=request.Nome, 
-                Descricao=request.Descricao, 
-                CEST=request.CEST,
-                NCM= request.NCM, 
-                QtdItensContidos = request.QtdItensContidos,
-                CodigoBarras= request.CodigoBarras,
-                MarcaId= request.MarcaId,
-                Tipo= request.Tipo,
-                UnidadeMedida= request.UnidadeMedida 
-            };
+            var validation = await _validator.ValidateAsync(request);
+            if (!validation.IsValid)
+            {
+                throw new ValidationException(validation.Errors);
+            }
+            var produto = _mapper.Map<Produtos.Domain.Entities.Produto>(request);
             var idProduto= await _produtoRepository.Inserir(produto);
             return idProduto;
         }
